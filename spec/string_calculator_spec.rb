ec53f20035fa384c886741e7462e922b29b3f83a
Shriyansh Neema <shriyanshneema31@gmail.com>
2025-09-07 00:33:26 +0530

    Added a test case for add method to returns the number itself if a given string is a single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe StringCalculator do
     it "returns 0 if a string is empty" do
       expect(StringCalculator.add("")).to eq(0)
     end
+
+    it "returns the number itself if a given string is a single number" do
+      expect(StringCalculator.add("1")).to eq(1)
+    end
   end
 end
