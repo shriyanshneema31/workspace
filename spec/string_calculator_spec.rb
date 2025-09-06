0e6b8f98017422cb8055f1270d329a7e49ccc491
Shriyansh Neema <shriyanshneema31@gmail.com>
2025-09-07 01:15:15 +0530

    Added a test case for add method to supports a custom delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -17,5 +17,9 @@ RSpec.describe StringCalculator do
     it "returns the sum of multiple numbers if separated by commas and newlines" do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
+
+    it "should supports a custom delimiter" do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
